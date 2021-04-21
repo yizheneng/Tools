9b80e23f6f9baf362dedfda09ff4234a22af907e
kangbo <kangbo0303@163.com>
2021-04-21 15:02:13 +0800

    [dev] 修改扫描方式为行列式

--- Picture/pictureToLcd.py
+++ Picture/pictureToLcd.py
@@ -1,6 +1,6 @@
 import os
 from PIL import Image
-imageSize = (32, 32)
+imageSize = (64, 64)
 threshold = 200
 table = []
 
@@ -10,7 +10,7 @@ for i in range(256):
     else:
         table.append(1)
 
-numberStr = "uint8_t picture%dX%d[][] = {\r\n"%imageSize
+numberStr = "const uint8_t picture%dX%d[][%d] = {\r\n"%(imageSize[0], imageSize[1], imageSize[0]*imageSize[1]/8)
 for root, dirs, files in os.walk("C:/Users/bing/Desktop/wather_png1"):
     for f in files:
         # print(os.path.join(root, f))
@@ -23,22 +23,41 @@ for root, dirs, files in os.walk("C:/Users/bing/Desktop/wather_png1"):
         numberStr += "  {"
 
         w1, h1 = img.size
-        for y in range(h1):
-            bitCount = 0
+
+        # for y in range(h1):
+        #     bitCount = 0
+        #     tempByte = 0
+        #     for x in range(w1):
+        #         pixel = img.getpixel((x, y)) 
+        #         if pixel == 0:
+        #             tempByte = tempByte | (1 << bitCount)
+        #         else:
+        #             tempByte = tempByte | (0 << bitCount)
+                
+        #         bitCount = bitCount + 1
+        #         if bitCount > 7 :
+        #             numberStr = numberStr + hex(tempByte) + ", "
+        #             bitCount = 0
+        #             tempByte = 0
+
+        tempY = int(h1 / 8)
+        for y in range(tempY):
             tempByte = 0
             for x in range(w1):
-                pixel = img.getpixel((x, y)) 
-                if pixel == 0:
-                    tempByte = tempByte | (1 << bitCount)
-                else:
-                    tempByte = tempByte | (0 << bitCount)
-                
-                bitCount = bitCount + 1
-                if bitCount > 7 :
-                    numberStr = numberStr + hex(tempByte) + ", "
-                    bitCount = 0
-                    tempByte = 0 
+                for i in range(8):
+                    pixel = img.getpixel((x, y * 8 + i)) 
+                    if pixel == 0:
+                        tempByte = tempByte | (1 << i)
+                    else:
+                        tempByte = tempByte | (0 << i)
+
+                numberStr = numberStr + hex(tempByte) + ", "
+                tempByte = 0 
+
         numberStr = numberStr[:-2]
         numberStr = numberStr + "}, // " + f + "\r\n"
-    numberStr = numberStr + "}"
+    numberStr = numberStr + "};"
 print(numberStr)
+
+file_handle=open('weathericon.h',mode='w')
+file_handle.write(numberStr)
